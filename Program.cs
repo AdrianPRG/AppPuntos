7b3308938a5c11fbb92f9474048f707034d5a6b9
AdrianPRG <alopgal962@g.educaand.es>
2023-12-17 19:49:31 +0100

    haciendolo bien

--- Program.cs
+++ Program.cs
@@ -1,6 +1,6 @@
 ﻿using System;
+/**
 GraficoCompuesto GraficoCOM = new GraficoCompuesto();
-        
 Punto punto = new Punto(600, 50);
 Console.WriteLine(punto.Dibujar());
 punto.Mover(3, 45);
@@ -16,6 +16,81 @@ GraficoCOM.añadir(circulo);
 GraficoCOM.añadir(rectangulo);
 
 GraficoCOM.mostrar();
+*/
+
+static void imprimeono(Punto punto)
+{
+    Console.WriteLine("DESEA SALIR (1) O DIBUJAR LA FIGURA (2)");
+    int quedesea = Convert.ToInt32(Console.ReadLine());
+    switch (quedesea)
+    {
+        case 1:
+            punto.Dibujar();
+            break;
+        case 2:
+            break;
+    }
+}
+
+static void main()
+{
+    bool continuar = true;
+    while (continuar!=false)
+    {
+        Console.WriteLine("QUE DESEA CREAR \n 1 -> PUNTO \n 2 -> CIRCULO \n 3 -> RECTANGULO");
+        int opcion = Convert.ToInt32(Console.ReadLine());
+        
+        switch (opcion)
+        {
+            case 1:
+                
+                Console.WriteLine("INTRODUZCA X");
+                int xdepunto = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("INTRODUZCA Y");
+                int ydepunto = Convert.ToInt32(Console.ReadLine());
+                Punto puntonuevo = new Punto(xdepunto, ydepunto);
+                puntonuevo.Mover(xdepunto, ydepunto);
+                imprimeono(puntonuevo);
+                break;
+            case 2:
+                Console.WriteLine("INTRODUZCA X");
+                int xdecirculo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("INTRODUZCA Y");
+                int ydecirculo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("INTRODUZCA RADIO");
+                int radiocirculo = Convert.ToInt32(Console.ReadLine());
+                Circulo circulonuevo = new Circulo(xdecirculo, ydecirculo,radiocirculo);
+                circulonuevo.Mover(xdecirculo, ydecirculo);
+                imprimeono(circulonuevo);
+                break;
+            case 3:
+                Console.WriteLine("INTRODUZCA X");
+                int xderectangulo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("INTRODUZCA Y");
+                int yderectangulo = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("INTRODUZCA ANCHO");
+                int ancho = Convert.ToInt32(Console.ReadLine());
+                Console.WriteLine("INTRODUZCA ALTO");
+                int alto = Convert.ToInt32(Console.ReadLine());
+                Rectangulo rectangulonuevo = new Rectangulo(xderectangulo, yderectangulo,ancho,alto);
+                rectangulonuevo.Mover(xderectangulo, yderectangulo);
+                imprimeono(rectangulonuevo);
+                break;
+            case 4:
+                continuar = false;
+                Console.WriteLine("PROGRAMA FINALIZADO");
+                break;
+        } 
+    }
+}
+
+main();
+
+
+//CLASES
+
+
+
 public class EditorGrafico{};
 
 //Interfaz Igrafico
@@ -43,15 +118,18 @@ public class Punto : IGrafico
     }
 
     //funcion mover con codigo
+    //Una clase que implemente una interfaz no tiene que hacer override de las funciones de la interfaz pues ya esta implementada
+    //virtual es para que los de abajo lo puedan heredar
+    //override es de clase a clase
     public virtual bool Mover(int x,int y)
     {
         if (x<0 || x>800)
         {
-            return false;
+            throw new Exception("X FUERA DE RANGO");
         }
         else if (y<0 || y>600)
         {
-            return false;
+            throw new Exception("Y FUERA DE RANGO");;
         }
         else return true;
     }
@@ -81,9 +159,9 @@ public class Circulo : Punto
     //fun override de Mover, con distinto codigo
     public override bool Mover(int x, int y)
     {
-        if (x + radio < 0 || x + radio > 800 && y + radio < 0 || y + radio > 600)
+        if (x - radio < 0 || x + radio > 800 || y - radio < 0 || y + radio > 600)
         {
-            return false;
+            throw new Exception("SE PASA DE LO PERMITIDO");
         }
         else return true;
     }
@@ -113,7 +191,7 @@ public class Rectangulo : Punto
     //fun override de mover con distinto codigo
     public override bool Mover(int x, int y)
     {
-        if (x + anchura < 0 || x + anchura > 800 && y + altura < 0 || y + altura > 600)
+        if (x - anchura/2 < 0 || x + anchura/2 > 800 || y - altura/2 < 0 || y + altura/2 > 600)
         {
             return false;
         }
